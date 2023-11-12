e05ade265b361c1a11119277d6af65bd05d418a4
Riten1 <122423532+Riten1@users.noreply.github.com>
2023-11-12 14:08:06 +0545

    'debug'

--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -35,7 +35,6 @@ function TodoItem({ todo }) {
         onChange={(e) => setTodoMsg(e.target.value)}
         readOnly={!isEdit}
       />
-      {/* Edit, Save Button */}
       <button
         className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
         onClick={() => {
